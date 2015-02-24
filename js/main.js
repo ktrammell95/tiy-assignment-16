605c0b3fa863e374eef9828c5ab312d9d72cc923
Katherine Trammell <ktrammell@gmail.com>
2015-02-24 17:45:35 -0500

    animation works

--- js/main.js
+++ js/main.js
@@ -6,14 +6,14 @@ $(function(){
         var preview = new Section(title);
         $el.append( preview.render() );
       });
-    $(".header").on("click", function() {
-    $(".paragraph").hide(1000);
-      });
 
-    $(".header").on("click", function() {
-    $(".paragraph").show();
-      });
+    $("h2").on("click", function(e) {
+       var title = $(e.currentTarget);
+       $(".paragraph").hide(2000);
+       title.siblings(".paragraph").show(2000);
+     });
     },
+
     error: function() {
       console.log("failed to load data.json");
       // console.log(a,b,c);
