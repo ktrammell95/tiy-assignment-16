8894645cb35be108674dc2b4623be15230512923
Katherine Trammell <ktrammell@gmail.com>
2015-02-24 22:58:35 -0500

    After working product updates
    Added images
    Updated text
    Added CSS styling

--- js/templates.js
+++ js/templates.js
@@ -3,7 +3,11 @@ this["JST"]["page-info"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"]
   var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
   return "<div class=\"content\">\n   <h2 class=\"header\">"
     + escapeExpression(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"title","hash":{},"data":data}) : helper)))
-    + "</h2>\n   <p class=\"paragraph\">"
+    + "</h2>\n   <div class=\"paragraph\">\n    <div class=\"pic\">\n      <img src=\""
+    + escapeExpression(((helper = (helper = helpers.img_src || (depth0 != null ? depth0.img_src : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"img_src","hash":{},"data":data}) : helper)))
+    + "\" alt=\""
+    + escapeExpression(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"title","hash":{},"data":data}) : helper)))
+    + "\">\n      <p>"
     + escapeExpression(((helper = (helper = helpers.paragraph || (depth0 != null ? depth0.paragraph : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"paragraph","hash":{},"data":data}) : helper)))
-    + "</p>\n</div>";
+    + "</p>\n    </div>  \n   </div>\n\n</div>";
 },"useData":true});
